bc0199c284956ac49102144bcf43df6e78542724
Daniel Emehinola <dan.emehin@gmail.com>
2026-05-15 18:47:13 -0400

    stage complete

--- app/main.py
+++ app/main.py
@@ -8,9 +8,12 @@ def main():
     while True:
         print("$ ", end="")
         user_input: str = input()
-        if user_input == "exit":
+        if user_input.startswith("echo "):
+            print(user_input[5:])
+        elif user_input == "exit":
             break
-        print(f"{user_input}: command not found")
+        else:
+            print(f"{user_input}: command not found")
 
 
 if __name__ == "__main__":
