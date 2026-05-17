7f5c8ff6e24868a89651e35ad85cb53feff0969a
Daniel Emehinola <dan.emehin@gmail.com>
2026-05-17 15:01:30 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 import sys
 
 
-def main():
+def main() -> None:
     # TODO: Uncomment the code below to pass the first stage
     # sys.stdout.write("$ ")
     # print("$ ", end="")
+    shell_builtin: list[str] = ["echo", "exit", "type"]
     while True:
         print("$ ", end="")
         user_input: str = input()
-        if user_input.startswith("echo "):
+        input_args: list[str] = user_input.split(" ")
+
+        if user_input.startswith("type "):
+            if input_args[1] in shell_builtin:
+                print(f"{input_args[1]} is a shell builtin")
+            else:
+                print(f"{input_args[1]}: not found")
+                continue
+
+        elif user_input.startswith("echo "):
             print(user_input[5:])
         elif user_input == "exit":
             break
