9470ec01589a3eb704f0e5f07402b72b7483bf66
Daniel Emehinola <dan.emehin@gmail.com>
2026-05-28 09:14:22 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,10 +6,9 @@ def main() -> None:
     # TODO: Uncomment the code below to pass the first stage
     # sys.stdout.write("$ ")
     # print("$ ", end="")
-    shell_builtin: list[str] = ["echo", "exit", "type", "pwd"]
+    shell_builtin: list[str] = ["echo", "exit", "type", "pwd", "cd"]
     sys_path: str | None = os.environ.get("PATH")
     path_list = []
-    executed: bool = False
 
     while True:
         print("$ ", end="")
@@ -18,16 +17,20 @@ def main() -> None:
         if sys_path:
             path_list: list[str] = sys_path.split(os.pathsep)
 
-        if user_input:
-            if input_args[0] not in shell_builtin:
-                for path in path_list:
-                    exec_path: str = os.path.join(path, input_args[0])
-                    if os.path.exists(exec_path) and os.access(exec_path, os.X_OK):
-                        subprocess.run(input_args)
-                        executed = True
-                        break
-
-        if user_input.startswith("type "):
+        if user_input == "exit":
+            break
+        elif user_input.startswith("echo "):
+            print(user_input[5:])
+        elif user_input.startswith("cd "):
+            abs_path = input_args[1]
+            try:
+                os.chdir(abs_path)
+            except OSError:
+                print(f"cd: {abs_path}: No such file or directory")
+        elif user_input == "pwd":
+            print(os.getcwd())
+            # continue
+        elif user_input.startswith("type "):
             if input_args[1] in shell_builtin:
                 print(f"{input_args[1]} is a shell builtin")
             elif input_args[1] not in shell_builtin:
@@ -38,16 +41,15 @@ def main() -> None:
                         break
                 else:
                     print(f"{input_args[1]}: not found")
-
-        elif user_input.startswith("echo "):
-            print(user_input[5:])
-        elif user_input == "exit":
-            break
-        elif user_input == "pwd":
-            print(os.getcwd())
-        else:
-            if executed is False:
-                print(f"{user_input}: command not found")
+                    # continue
+        elif input_args[0] not in shell_builtin:
+            for path in path_list:
+                exec_path: str = os.path.join(path, input_args[0])
+                if os.path.exists(exec_path) and os.access(exec_path, os.X_OK):
+                    subprocess.run(input_args)
+                    break
+            else:
+                print(f"{input_args[0]}: command not found")
 
 
 if __name__ == "__main__":
